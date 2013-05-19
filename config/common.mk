e265199d26d12b7c42c720d119bbd204728cfb61
Nick0703 <lokesh.c703@gmail.com>
2013-05-19 16:30:18 -0400

    Fix typo
    Change-Id: Id6f92fa9e18104eb0a4db136a8f224e917f4530c

--- config/common.mk
+++ config/common.mk
@@ -27,8 +27,8 @@ ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
-    vendor/cm/CHANGELOG.mkdn:system/etc/PAC-CHANGELOG.txt
-	vendor/cm/CONTRIBUTORS.mkdn:system/etc/PAC-CONTRIBUTORS.txt
+    vendor/cm/CHANGELOG.mkdn:system/etc/PAC-CHANGELOG.txt \
+    vendor/cm/CONTRIBUTORS.mkdn:system/etc/PAC-CONTRIBUTORS.txt
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
